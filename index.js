22242f84a0f400366dfd60aed9216badcc3d7669
Dejanee Gomez <dejaneegomez@gmail.com>
2023-12-19 13:05:57 -0500

    adds loopingcode

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+
+function writeCards(names, event) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+console.log(writeCards(names, event));
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+}
+}
+countDown(10);
+
